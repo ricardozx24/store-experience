bde8fa4caafdafffae66c79884d333e1b5a2682b
ricardozx24 <142248471+ricardozx24@users.noreply.github.com>
2023-10-05 09:42:47 -0500

    first commit store experience

---
+++ src/Components/Hero.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+
+        <section className="hero">
+                <img src="images/store-arrival.jpg" className="hero--photo" alt="house1" />
+                <img src="images/forney-store-team.jpg" className="hero--photo" alt="house2" />
+                <img src="images/parking-lot-walk.jpg" className="hero--photo" alt="house3" />
+                <img src="images/battery-test.jpg" className="hero--photo" alt="house4" />
+                <img src="images/alternator-test.jpg" className="hero--photo" alt="house5" />
+                <img src="images/putting-up-truck.jpg" className="hero--photo" alt="house6" />
+                <img src="images/sku-check.jpg" className="hero--photo" alt="house6" />
+                <img src="images/cycle-counting-batteries.jpg" className="hero--photo" alt="house6" />
+            <h1 className="hero--header">In Store Experience Education</h1>
+            <p className="hero--text">Had an amazing experience at the Forney Texas AutoZone Store, great team. Did a little bit of everything, walked parking lot, picked up trash, sales counter, checkout, cycle counts, manager next day review, battery testing and installation, alternator testing, put up truck, updated floor planogram.</p>
+            
+            
+        </section>
+    )
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="images/autozone-sign.jfif" alt="AutoZone Logo" />
+            
+        </nav>
+    )
+}
